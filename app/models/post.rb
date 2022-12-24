7159b7b041dcdb9988863e370b599aad4e10306e
Ryan DeLong <rdelong111@gmail.com>
2022-12-24 16:27:33 -0600

    commit tree 7e8bfcc9fa02af49b22e0f6da81c86962e9eefb9

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,13 @@
 class Post < ApplicationRecord
+  validates :title, presence: true
+  validates :content, length: { minimum: 250 }
+  validates :summary, length: { maximum: 250 }
+  validates :category, inclusion: { in: %w(Fiction Non-Fiction) }
+  validate :contains_clickbait
+
+  def contains_clickbait
+    unless (/Won't Believe|Secret|Top \d+|Guess/).match? title
+      errors.add(:title, "Not enough clickbait.")
+    end
+  end
 end
